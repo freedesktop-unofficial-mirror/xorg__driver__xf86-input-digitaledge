3f17d5120f55d5c1923362168db27ddaebacdc18
Daniel Stone <daniel@fooishbar.org>
2005-07-03 08:53:45 +0000

    Change <X11/misc.h> and <X11/os.h> to "misc.h" and "os.h".

--- src/DigitalEdge.c
+++ src/DigitalEdge.c
@@ -46,7 +46,7 @@
 #include <errno.h>
 #endif
 
-#include <X11/misc.h>
+#include "misc.h"
 #include "xf86.h"
 #define NEED_XF86_TYPES
 #if !defined(DGUX)
@@ -105,7 +105,7 @@ static InputDriverPtr dedgeDrv;
 #define NEED_EVENTS
 #include <X11/X.h>
 #include <X11/Xproto.h>
-#include <X11/misc.h>
+#include "misc.h"
 #include "inputstr.h"
 #include "scrnintstr.h"
 #include <X11/extensions/XI.h>
